ed1557fc81f7af40ec55d86a82f9fa61d160fea0
DlanFeliacci <adlafeliachi@gmail.com>
2023-08-01 15:11:09 +0100

    save5

--- src/app/components/Service.jsx
+++ src/app/components/Service.jsx
@@ -32,31 +32,44 @@ const Service = () => {
           <div className="flex flex-wrap">
           <ServiceCard
               title="Graphic Creation"
-              details="We dejoy working with discerning clients, people for whom qualuty, service, integrity & aesthetics."
+              details="Our communication and advertising agency offers you these communication strategy consulting services which also include
+              all of the production and implementation of all your communication operations.
+              We will support you in all your graphic design projects, website design, digital transformation, printing
+              digital & offset, Screen printing, Signage, Printing on Advertising Objects, Textiles... "
               icon={<Image src={Logo}/>}
             />
             <ServiceCard
               title="Printing"
-              details="We dejoy working with discerning clients, people for whom qualuty, service, integrity & aesthetics."
+              details="Has sed verum sed aptae medelarum aquae aquae his usus calentes navigerum has usus locis natura speciem captis plurimis ad Pompeius flumen medelarum et verum sorte has suapte pari dictione aquae emergunt iuris navigerum suapte regiones locis in delata flumen delata sorte emergunt his locis tractibus Iudaeis suapte provinciae sed."
               icon={<Image src={PrinterIcon}/>}
             />
             <ServiceCard
               title="Signage System"
-              details="We dejoy working with discerning clients, people for whom qualuty, service, integrity & aesthetics."
+              details="Company signage or professional signage refers to all of the means likely to indicate, inform or
+              guide customers, employees and visitors to a point of sale or company premises. Report and vital for the company.
+              Especially when you know that the first impression will never leave your visitors.
+              Whether in terms of graphic design or the message to be conveyed, good signage has a significant impact on customers.
+              and serves as an advertising office at a lower cost compared to conventional advertisements.
+              To show a communication in accordance with your image, we will provide you with several solutions that will help you to
+              better promote your Hotel."
               icon={<Image src={Signage}/>}
             />
             <ServiceCard
               title="Digital Marketing"
-              details="lorem ipsum havus boro drem"
+              details="The creation of websites, mobile apps, software and digital transformation is a job that requires expertise and mobilization
+              of a whole team of professional and creative developers. Your website is the identity and the backbone of your communication.
+              To carry out a web project, you must structure your offer, present your company and work on your commercial speech."
               icon={<Image src={Webdesign}/>}
             />
             <ServiceCard
               title="Audiovisual Production"
-              details="We dejoy working with discerning clients, people for whom qualuty, service, integrity & aesthetics."
+              details="Photos and videos make it possible to capture and fix the attention immediately because as the expression says: « an image is better than a thousand.
+              words ! »
+              We identify and analyze your target in order to make your message effective and persuasive to highlight your strengths and specificities. So make an impression with fast, efficient and economical communication."
               icon={<Image src={Photography}/>}
             />
             <ServiceCard
-              title="Packaging"
+              title="Other"
               details="We dejoy working with discerning clients, people for whom qualuty, service, integrity & aesthetics."
               icon={<Image src={Catalogue}/>}
             />
@@ -81,7 +94,7 @@ const ServiceCard = ({ icon, title, details }) => {
             {icon}
           </div>
           <h4 className="text-center text-[#534092] mb-3 text-3xl font-semibold text-dark">{title}</h4>
-          <p className="text-center text-body-color">{details}</p>
+          <p className="text-body-color text-justify">{details}</p>
         </div>
       </div>
     </>
