1a61c81c1f86d56004440cbf945b09acc5d6ce26
DlanFeliacci <adlafeliachi@gmail.com>
2023-08-10 10:46:05 +0100

    save9

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     <>
     <div className='background-contact max-w-full h-[60vh] md:h-[100vh]'></div>
     <div className="animateCss">
-        <Lottie animationData={contactUs} style={{maxWidth: '40%'}} className="absolute top-[64px] right-0"/>
+        <Lottie animationData={textAnimation} style={{maxWidth: '40%'}} className="absolute top-[64px] right-0"/>
     </div>
     <div className='flex flex-col sm:flex-row justify-evenly py-[60px] p-2 bg-[#F7F7F7]'>
         <div className='lg:w-[800px] flex flex-col justify-evenly '>
